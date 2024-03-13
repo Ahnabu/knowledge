840af03c24e6eba9af1d33295659416d2d0a5fb2
Ahnabu <horairaabu013025@gmail.com>
2024-03-13 08:42:12 +0600

    second commit

--- react-knowledge-cafe/src/App.jsx
+++ react-knowledge-cafe/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header></Header>
       
-      <h1 className='text-6xl'>Knowledge</h1>
-      
+      <Blogs></Blogs>
       
     </>
   )
---
+++ react-knowledge-cafe/src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Knowledge CAFE</h1>
+        </div>
+    );
+};
+
+export default Header;
